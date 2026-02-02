077067c8f140128443c169f1844cb6cedd9499f1
Mihirr13 <mihir312005@gmail.com>
2026-02-02 11:51:55 +0530

    2 feb tasks

---
+++ Frontend/learning2026/src/component/MapDemo6.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MapDemo6 = () => {
+  return (
+    <div>MapDemo6</div>
+  )
+}
---
+++ Frontend/learning2026/src/component/MapDemo7.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const s = () => {
+  return (
+    <div>s</div>
+  )
+}
